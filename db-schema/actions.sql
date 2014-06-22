8e1a09ef7544f38dff11875d24c4675b9aef43f2
Jason <jasonhoekstra@gmail.com>
2014-06-22 14:05:00 -0400

    better syntax for creating PK on actions

--- db-schema/actions.sql
+++ db-schema/actions.sql
@@ -2,10 +2,8 @@
 --  Table structure for "public"."actions"
 -- ----------------------------
 
-CREATE SEQUENCE actions_action_id_seq START 1;
-
 CREATE TABLE "public"."actions" (
-	"action_id" int4 NOT NULL DEFAULT nextval('actions_action_id_seq'::regclass),
+	"action_id" SERIAL PRIMARY KEY,
 	"user_id" varchar(1024),
 	"action_name" varchar(1024),
 	"source_url" varchar(1024),
@@ -18,9 +16,3 @@ CREATE TABLE "public"."actions" (
 )
 WITH (OIDS=FALSE);
 ALTER TABLE "public"."actions" OWNER TO "jason";
-
--- ----------------------------
---  Primary key structure for table "public"."actions"
--- ----------------------------
-ALTER TABLE "public"."actions" ADD CONSTRAINT "actions_pkey" PRIMARY KEY ("action_id") NOT DEFERRABLE INITIALLY IMMEDIATE;
-

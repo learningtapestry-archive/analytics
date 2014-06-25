2ce4c3a263eafaffc246ec9a00e48e1cb0a9b2c8
Jason <jasonhoekstra@gmail.com>
2014-06-25 12:09:56 -0400

    updates for d3 visualization

--- dashboard/data.php
+++ dashboard/data.php
@@ -14,10 +14,38 @@
 	}
 
 	if ($connectionSuccess) {
-		$sql = "SELECT * FROM actions ORDER BY insert_date";
+		$sql = "SELECT * FROM vw_users_hits;";
+
+		$first = true;
 
     	foreach ($connection->query($sql) as $row) {
-    		print_r($row);a
+
+    		if ($first) {
+	    		foreach ($row as $key=>$value) {
+	    			if (!is_numeric($key)) {
+		    			echo $key;
+		    			echo "\t";
+	    			}
+	    		}
+	    		echo "\n";
+	    		foreach ($row as $key=>$value) {
+	    			if (!is_numeric($key)) {
+		    			echo $value;
+		    			echo "\t";
+	    			}
+	    		}
+	    		$first = false;    			
+    		} else {
+				foreach ($row as $key=>$value) {
+    			if (!is_numeric($key)) {
+	    			echo $value;
+	    			echo "\t";
+    				}
+	    		}
+    		}
+
+    		echo "\n";
+//    		echo "<br/>";
     	}
 	}
 ?>

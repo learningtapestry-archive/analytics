3b71007b0851017358555ae9ffa543ce47a19b0f
Jason Hoekstra <jasonhoekstra@gmail.com>
2014-08-03 13:33:59 -0400

    Now pushing POST data to Redis queue

--- nginx-config/lt_lua_modules/api_key_validate.lua
+++ nginx-config/lt_lua_modules/api_key_validate.lua
@@ -7,6 +7,7 @@
 --        but close enough for fast GUID flash test (must be hex with four dashes + below string len 
 --        of 36 chars)
 GUID_MATCH = "%x+%-%x+%-%x+%-%x+%-%x+"
+LUA_FILE_NAME = "api_key_validate.lua"
 
 if ngx.req.get_method() ~= "POST" or 
    ngx.req.get_headers()["X-LT-API-Key"] == nil or
@@ -22,19 +23,23 @@ else
 	-- Connect to Redis at localhost
 	local ok, err = red:connect("127.0.0.1", 6379)
         if not ok then
-        	ngx.log(ngx.ERR, "failed to connect to redis: ", err)
+        	ngx.log(ngx.ERR, LUA_FILE_NAME .. ": failed to connect to Redis: ", err)
         	return ngx.exit(500)
         end
 
 	-- Retrieve API key
 	local queue, err = red:hget(api_key, "queue")
         if not queue or queue == ngx.null then
-        	-- ngx.log(ngx.ERR, "API key does not exist: " .. api_key, err)
+        	-- ngx.log(ngx.ERR, "API key does not exist: " .. api_key, err) 
         	return ngx.exit(401)
 	else
-		ngx.say("API key and queue exists")
-		ngx.say(queue)
-		ngx.say(api_key)
-		return ngx.exit(200)
+		ngx.req.read_body() -- necessary to access get_body_data()
+		local result, err = red:lpush(queue, ngx.req.get_body_data())
+		if not result then
+			ngx.log(ngx.ERR, LUA_FILE_NAME .. ": Redis LPUSH failed to: " .. queue, errr)
+			return ngx.exit(400)
+		else
+			return ngx.exit(200)
+		end
         end
 end

0fc3350269da5e916b6529d7d8e87da70a6454e1
Jason <jasonhoekstra@gmail.com>
2014-08-08 16:40:59 -0400

    Refactored to use Activerecord

--- ruby-janitors/redis-to-postgres-html-dequeue.rb
+++ ruby-janitors/redis-to-postgres-html-dequeue.rb
@@ -17,11 +17,17 @@ require 'json'
 require 'pg'
 require 'redis'
 require 'logger'
+require 'rubygems'
+require 'active_record'
+require 'yaml'
+
+class RawMessage < ActiveRecord::Base
+end
 
 logger = Logger.new($stdout)
 
 # Read configuration from file, store in config variable (hash)
-config = JSON.parse(File.read('config.json'))
+config = JSON.parse(File.read(CONFIG_FILE))
 
 # If configuration doesn't parse or doesn't contain first element, stop and throw error
 if !config || !config["configuration"] then
@@ -41,11 +47,23 @@ end
 
 # Connect to Postgres
 begin
-	pg = PG::Connection.open(config["configuration"]["postgres_connection_string"])
+	dbconfig = YAML::load(File.open('db/config.yml'))
+	ActiveRecord::Base.establish_connection(dbconfig['development'])
 rescue Exception => e
-	logger.error("Cannot connect to Postgres, connect string: " + config["configuration"]["postgres_connection_string"] + 
+	logger.error("Cannot connect to Postgres, connect string: " + dbconfig['development'] + 
 		", error: " + e.message)
 	abort()
 end
 
+
+#=begin
 logger.info "I can connect to both Redis and Postgres, now ready to do some work."
+
+sample_message = RawMessage.new
+sample_message.status = "OK"
+sample_message.api_key = "123"
+sample_message.email = "jasonhoekstra@gmail.com"
+sample_message.action = "viewed/view"
+sample_message.save
+puts sample_message.id
+#=end

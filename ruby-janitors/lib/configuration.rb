5c321ed483ebd3d7ed57771d4c62cf8f437ad5a4
Jason <jasonhoekstra@gmail.com>
2014-08-13 08:39:54 -0400

    adding configuration class

---
+++ ruby-janitors/lib/configuration.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/env ruby
+
+# Copyright 2014 Learntaculous (Hoekstra/Midgley) - All Rights Reserved
+
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+
+class Configuration
+
+	@is_initialized = false # this will remain false until config loaded successfully
+	@error_message = ""
+
+	def initialize(config_file)
+		# Read configuration from file, store in config variable (hash)
+		config = JSON.parse(File.read(config_file))
+
+		# If configuration doesn't parse or doesn't contain first element, stop and throw error
+		if !config || !config["configuration"] then
+			@error_message = "Configuration file '#{config_file}' not found or invalid format."
+		else
+			begin
+				@redis_url = config["configuration"]["redis_url"]
+				@redis_queue_name = config["configuration"]["redis_queue_name"]
+				@message_error_dir = config["configuration"]["message_error_dir"]
+				@message_dberror_dir = config["configuration"]["message_dberror_dir"]
+
+				# Ensure error directories exist, error out if no, normalize paths if so
+				if File.directory?(@message_error_dir) && File.directory?(@message_dberror_dir) then
+					@message_error_dir = File.expand_path(@message_error_dir)
+					@message_dberror_dir = File.expand_path(@message_dberror_dir)
+
+					@is_initialized = true
+				else
+					@error_message = "Message error directories do not exist, error dir: #{@message_error_dir} and db error dir: #{@message_dberror_dir}"
+				end
+				
+			rescue Exception => e
+				@error_message = e.message
+			end
+		end
+	end
+
+	def redis_url
+		@redis_url
+	end
+
+	def redis_queue_name
+		@redis_queue_name
+	end
+
+	def message_error_dir
+		@message_error_dir
+	end
+
+	def message_dberror_dir
+		@message_dberror_dir
+	end
+
+	def error_message
+		@error_message
+	end
+
+	def is_initialized
+		@is_initialized
+	end
+end

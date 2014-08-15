5490a3ebd9e5ab308e4264694d8e2b02b577a97c
Jason <jasonhoekstra@gmail.com>
2014-08-15 00:26:56 -0400

    Now extracting information from raw_messages table into statement table

--- ruby-janitors/postgres-html-info-extract.rb
+++ ruby-janitors/postgres-html-info-extract.rb
@@ -20,6 +20,8 @@ require 'nokogiri'
 require './lib/util/configuration.rb'
 require './lib/model/raw_message.rb'
 require './lib/model/statement.rb'
+require './lib/model/approved_site.rb'
+require './lib/model/extraction_map.rb'
 
 logger = Logger.new($stdout)
 
@@ -34,19 +36,78 @@ end
 
 raw_messages = RawMessage.where(status: "READY")  #TODO:  Consider putting limit to not grab unmanagable amount of records
 
+approved_sites = ApprovedSite.all
+
+#approved_sites.each do |approved_site|
+#	puts approved_site.extraction_maps.inspect
+#end
+
 raw_messages.each do |raw_message|
 	html = Nokogiri.parse(raw_message.html)
-	subject_title = html.css('main article div section h1')
-	topic_segment = html.css('article.syllabus__topic')
+	hash_id = "gnqCcQuzkiGYOOo3"  #TODO: embed into Chrome Extension, hardcoding until
 
-	topic_segment.each do |snippet|
-		lesson_title = snippet.css('h4')
-		value = snippet.css('span.syllabus__progress')
+	approved_site = approved_sites.find_by hash_id: hash_id
 
-		puts subject_title[0].content
-		puts raw_message.url
-		puts lesson_title[0].content
-		puts value[0]['value']
-		puts "-------------------"
+	if approved_site then
+		begin
+
+
+			# Check if our extraction map is an iterator, if so, iterate statement, if not, extract one statement
+			if approved_site.extraction_maps.where("parent_extraction_map_id IS NOT NULL").count > 0  &&
+				approved_site.extraction_maps.where("target_field IS NULL AND parent_extraction_map_id IS NULL").count == 1 then
+
+				iterator_map = approved_site.extraction_maps.where("target_field IS NULL AND parent_extraction_map_id IS NULL")
+
+				if iterator_map && iterator_map.count == 1 then
+					iterator = html.css(iterator_map[0].css_selector)
+
+					iterator.each do |snippet|
+						statement = Statement.new
+						statement.user_id = 1   #TODO: Get actual user_id
+						statement.actor = raw_message.email
+						statement.verb = raw_message.action
+
+						# Extract non-repeating elements
+						snippet_maps = approved_site.extraction_maps.where("parent_extraction_map_id IS NULL AND target_field IS NOT NULL")
+						snippet_maps.each do |snippet_map|
+							#print snippet_map.target_field + " - " + snippet_map.css_selector + " - "
+							#puts html.at_xpath(snippet_map.css_selector).content
+							statement[snippet_map.target_field] = html.at_xpath(snippet_map.css_selector).content
+						end
+
+						# Extract repeating elements
+						snippet_maps = approved_site.extraction_maps.where("parent_extraction_map_id IS NOT NULL AND target_field IS NOT NULL")
+						snippet_maps.each do |snippet_map|
+							#print snippet_map.target_field + " - " + snippet_map.css_selector + " - "
+							#puts snippet.at_xpath(snippet_map.css_selector).content
+							statement[snippet_map.target_field] = snippet.at_xpath(snippet_map.css_selector).content
+						end
+
+						statement.save
+						raw_message.status = "OK"
+						raw_message.save
+					end
+				else
+					statement = Statement.new
+					statement.user_id = 1   #TODO: Get actual user_id
+					statement.actor = raw_message.email
+					statement.verb = raw_message.action
+
+					# Extract non-repeating elements
+					snippet_maps = approved_site.extraction_maps.where("parent_extraction_map_id IS NULL")
+					snippet_maps.each do |snippet_map|
+						statement[snippet_map.target_field] = html.at_xpath(snippet_map.css_selector)
+					end
+
+					statement.save
+					raw_message.status = "OK"
+					raw_message.save
+				end
+			end
+		rescue Exception => e
+			logger.error("Error parsing raw message: #{raw_message.id}, error: #{e.message})")
+			raw_message.status = "ERROR"
+			raw_message.save
+		end
 	end
 end

442ddd4d9e92791b9e716fc392fcd65ab54742b8
Jason Hoekstra <jasonhoekstra@gmail.com>
2014-09-07 17:39:59 -0400

    changing class vars to instance vars

--- core-app/lib/util/redis_server.rb
+++ core-app/lib/util/redis_server.rb
@@ -12,15 +12,15 @@ module LT
           redis_config = YAML::load(File.open(config_file))
           # QUESTION:  Ask SM what is the right way to startup w/ environment path
           LT::setup_environment(".")
-          @redis_url = redis_config[LT::run_env]["url"]
+          redis_url = redis_config[LT::run_env]["url"]
           
           # Store static queue / hashlist names
-          @@queue_raw_message = redis_config[LT::run_env]["queue_raw_message"]
-          @@hashlist_api_keys = redis_config[LT::run_env]["hashlist_api_keys"]
+          @queue_raw_message = redis_config[LT::run_env]["queue_raw_message"]
+          @hashlist_api_keys = redis_config[LT::run_env]["hashlist_api_keys"]
           
           # Define and connect to server
-          @@redis = Redis.new(:url => @redis_url)
-          @@redis.ping # connect to Redis server
+          @redis = Redis.new(:url => @redis_url)
+          @redis.ping # connect to Redis server
         rescue Exception => e
           # TODO: Refactor logger to another module outside of Janitor namespace
           LT::Janitor::init_logger
@@ -30,40 +30,40 @@ module LT
       end
 
       def raw_messages_queue_clear
-        @@redis.del @@queue_raw_message
+        @redis.del @queue_raw_message
       end
 
       def raw_message_push(message)
-        @@redis.lpush @@queue_raw_message, message
+        @redis.lpush @queue_raw_message, message
       end
 
       def raw_message_pop
-        @@redis.rpop @@queue_raw_message
+        @redis.rpop @queue_raw_message
       end
 
       def raw_message_queue_length
-        @@redis.llen @@queue_raw_message
+        @redis.llen @queue_raw_message
       end
 
 
       def api_keys_hashlist_clear
-        @@redis.del @@hashlist_api_keys
+        @redis.del @hashlist_api_keys
       end
 
       def api_key_get(key)
-        @@redis.hget @@hashlist_api_keys, key
+        @redis.hget @hashlist_api_keys, key
       end
 
       def api_key_set(key, value)
-        @@redis.hset @@hashlist_api_keys, key, value
+        @redis.hset @hashlist_api_keys, key, value
       end
 
       def api_key_remove(key)
-        @@redis.hdel @@hashlist_api_keys, key
+        @redis.hdel @hashlist_api_keys, key
       end
 
       def api_key_hashlist_length
-        @@redis.hlen @@hashlist_api_keys
+        @redis.hlen @hashlist_api_keys
       end
 
     end

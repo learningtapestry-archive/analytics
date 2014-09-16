9e2821f64b35a5e78d7ab4d80134a461dfb69cc4
Jason Hoekstra <jasonhoekstra@gmail.com>
2014-09-16 08:11:09 -0400

    update URLs for Chrome extension

--- browser-plugins/chrome-extension/popup.js
+++ browser-plugins/chrome-extension/popup.js
@@ -18,8 +18,8 @@ var LoginManager = {
     
     msg_to: null,
     
-    login_url: 'http://localhost:8080/api/v1/login',
-    create_url: 'http://localhost:8080/api/v1/signup',
+    login_url: 'https://lt-dev01.learntaculo.us/api/v1/login',
+    create_url: 'https://lt-dev01.learntaculo.us/api/v1/signup',
     
     init: function() {
         this.bg = chrome.extension.getBackgroundPage();

e6445b848eac1a3d7d73c9d68967cd69ae6b54b7
Jason Hoekstra <jasonhoekstra@gmail.com>
2014-09-18 02:03:16 -0400

    implemented redis to sites/pages message mover janitored.  (almost finished state)

--- core-app/lib/janitors/redis_postgres_sites_mover.rb
+++ core-app/lib/janitors/redis_postgres_sites_mover.rb
@@ -8,101 +8,68 @@ module LT
 			class << self
 				def extract
 
+					# TODO:  Loop this
+					
+					redis_message = LT::RedisServer::raw_message_pop
 
+					if redis_message then
+						redis_message = JSON.parse(redis_message)
+
+						username = redis_message["user"]["username"]
+						api_key_message = redis_message["user"]["apiKey"]
+						site_hash = redis_message["user"]["site_hash"]
+						action = redis_message["user"]["action"]["id"]
+						timestamp = redis_message["user"]["timestamp"]
+						url = redis_message["user"]["url"]["id"]
+
+						api_key_postgres = ApiKey.get_by_api_key(api_key_message)
+
+						if api_key_postgres then
+							approved_site = ApprovedSite.get_by_site_hash(site_hash)
+
+							if approved_site then
+					      
+					      site = Site.where(:url=>approved_site.url).first
+					      if !site then
+					      	new_site = Site.create
+					      	new_site.url = approved_site.url
+					      	new_site.display_name = approved_site.site_name
+					      	new_site.save
+					      	site = new_site.dup
+					      end # site
+
+					      page = Page.where(:url=> url).first
+					      if !page then
+					      	new_page = Page.create
+					      	new_page.url = url
+					      	new_page.site_id = site.id
+					      	new_page.save
+					      	page = new_page.dup
+					      end # page
+
+					      begin 
+					      page_visit = PagesVisited.create
+					      page_visit.user_id = api_key_postgres.user_id
+					      page_visit.page_id = page.id
+					      page_visit.date_visited = timestamp
+					      if action == "verbs/viewed" then
+					      	page_visit.time_active = redis_message["user"]["action"]["value"]["time"]
+					      end #action
+					      page_visit.save
+
+						    rescue Exception => e 
+						    	puts e
+						    end
+
+					    else
+					    	puts "Invalid approved_site"
+					    end # approved_site
+						else
+							puts "Invalid API key"
+						end # api_key_postgres
+					end # redis_message
 				end # extract
 			end # class << self
 		end # RedisPostgresMover
 	end # Janitors
 end
-
-=begin
-
-CONFIG_FILE = "./config/config.json"
-
-require 'json'
-require 'pg'
-require 'redis'
-require 'logger'
-require 'rubygems'
-require 'active_record'
-require 'yaml'
-require 'htmlentities'
-require './lib/model/raw_message.rb'
-
-logger = Logger.new($stdout)
-html_decoder = HTMLEntities.new
-
-config = Configuration.new(File.expand_path(CONFIG_FILE)) # Load Configuration class with a normalized full file path
-
-# If configuration is not fully initialized, then log error and abort execution
-if !config.is_initialized then
-	logger.error("Configuration not initialized, error: #{config.error_message}")
-	abort()
-end
-
-# Connect to Redis
-redis = Redis.new(:url => config.redis_url)
-
-begin
-	redis.ping # connect to Redis server
-rescue Exception => e
-	logger.error("Cannot connect to Redis, connect url: #{redis_url}, error: #{e.message}")
-	abort()
-end
-
-# Connect to Postgres
-begin
-	dbconfig = YAML::load(File.open('db/config.yml'))
-	ActiveRecord::Base.establish_connection(dbconfig['development'])
-rescue Exception => e
-	logger.error("Cannot connect to Postgres, connect string: #{dbconfig['development']}, error: #{e.message}")
-	abort()
-end
-
-counter = 0 # For unique filenames upon error
-# If the Redis queue has messages, RPOP messages until empty
-if (redis.llen config.redis_queue_name) > 0 then
-	message = redis.rpop config.redis_queue_name
-	until message.nil?
-		begin
-			json_message = JSON.parse(message)
-			if json_message.key?("user") &&
-				json_message["user"].key?("email") &&
-				json_message["user"].key?("apiKey") &&
-				json_message["user"].key?("action") &&
-				json_message["user"]["action"].key?("id") &&
-				json_message["user"].key?("url") &&
-				json_message["user"]["url"].key?("id") then
-
-				raw_message = RawMessage.new
-				raw_message.status = "READY"
-				raw_message.email = json_message["user"]["email"]
-				raw_message.action = json_message["user"]["action"]["id"]
-				raw_message.api_key = json_message["user"]["apiKey"]
-				raw_message.url = json_message["user"]["url"]["id"]
-				raw_message.date_captured = json_message["user"]["timestamp"]
-				raw_message.date_created = Time.now
-
-				if json_message["user"].key?("html") then
-					raw_message.html = html_decoder.decode json_message["user"]["html"]
-				end
-
-				begin
-					raw_message.save
-				rescue Exception => e
-					file_name = Time.now.strftime('%Y%m%d%H%M%S%L') + "-#{counter}.json"
-					logger.warn("Cannot save message into database, dumping to #{message_dberror_dir}/#{file_name}, error: #{e.message}")
-					File.open(message_dberror_dir + '/' + file_name, 'w') {|f| f.write(message) } # Dump to disk
-				end
-			end
-		rescue Exception => e
-			file_name = Time.now.strftime('%Y%m%d%H%M%S%L') + "-#{counter}.json"
-			logger.warn("Cannot parse message into JSON, dumping to #{message_error_dir}/#{file_name}, error: #{e.message}")
-			File.open(message_error_dir + '/' + file_name, 'w') {|f| f.write(message) } # Dump to disk
-		end
-
-		message = redis.rpop redis_queue_name
-		counter += 1
-	end
-end
-=end
---
+++ core-app/test/janitors/redis_postgres_sites_mover_test.rb
@@ -0,0 +1,102 @@
+gem "minitest"
+require 'minitest/autorun'
+require 'debugger'
+require 'database_cleaner'
+require File::join(LT::lib_path, 'util', 'redis_server.rb')
+require File::join(LT::lib_path, 'util', 'session_manager.rb')
+require File::join(LT::lib_path, 'janitors', 'redis_postgres_sites_mover.rb')
+
+class RedisPostgresSitesMoverTest < Minitest::Test
+  def self.before_suite
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  def teardown
+    DatabaseCleaner.clean # cleanup of the database
+  end
+
+  def setup
+    # set database transaction, so we can revert seeds
+    DatabaseCleaner.start
+    LT::boot_all
+  end
+
+  def test_RedisPostgresViewedMessageMove
+    LT::RedisServer::api_keys_hashlist_clear
+    count = LT::RedisServer::api_key_hashlist_length
+    assert_equal 0, count
+
+    LT::RedisServer::raw_messages_queue_clear
+    count = LT::RedisServer::raw_message_queue_length
+    assert_equal 0, count
+
+    User.delete_all
+
+    # Create new user as target for new API key
+    username = 'test-user'
+    password = 'test-password'
+    first_name = 'First'
+    last_name = 'Last'
+    user = User.create_user(:username=>username, :password=>password, :first_name => first_name,
+      :last_name=>last_name)[:user]
+
+    # Ensure new user was created
+    assert_equal username, user.username
+    assert user.password_matches?(password)
+
+    # Use SessionManager to validate user and create new API key
+    api_key = LT::SessionManager.validate_user(username, password)
+
+    # Ensure we received back API key
+    match = /^[A-Za-z0-9]{8}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{12}$/.match(api_key)
+    refute_nil match
+
+    # Test in Redis that API key gives us a database name
+    db_name_redis = LT::RedisServer.api_key_get(api_key)
+    assert_equal LT.get_db_name, db_name_redis
+
+    # Test that API key from Redis matches user in Postgres
+    api_key_postgres = ApiKey.get_by_api_key(api_key)
+    assert_equal api_key_postgres.user_id, user.id
+
+    site_hash = "4f5978b72bf7f778629886a575375ba6"
+    url = "http://stackoverflow.com/search?q=redis+ruby"
+    visit_value = "1M32S"
+    timestamp = "2014-09-16T13:15:59-04:00"
+
+    raw_message =   { :user => {
+                            :username => username, 
+                            :apiKey => api_key,
+                            :site_hash => site_hash,
+                            :action => {
+                                :id => "verbs/viewed", 
+                                :display => { :en_us => "viewed" }, 
+                                :value => { :time => visit_value }
+                                }, 
+                            :url => { :id => url }, 
+                            :timestamp => timestamp
+                        }
+                    }
+
+    LT::RedisServer::raw_message_push(raw_message.to_json)
+
+    LT::Janitors::RedisPostgresSitesMover.extract
+    approved_site = ApprovedSite.get_by_site_hash(site_hash)
+    refute_nil approved_site
+
+    site = Site.where(:url => approved_site.url).first
+    refute_nil site
+    
+    page = Page.where(:site_id => site.id, :url => url).first
+    refute_nil page
+
+    page_visit = PagesVisited.where(:page_id => page.id, :user_id => user.id, :date_visited => timestamp).first
+    refute_nil page_visit
+    assert_equal page_visit.time_active, "00:01:32"
+  end
+
+  # TODO: implement
+  # def test_RedisPostgresClickedMessageMove
+  # end
+
+end

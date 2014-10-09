eb9db761251d62ac4194787235a4f61ea878ca89
Steve Midgley <science@misuse.org>
2014-10-08 18:22:39 -0700

    Removed field validations for User model org_api_key causes a new user to be auto-created for any new username that isn't already in our database. That auto-user doesn't have a password, first name or last name, all of which were required by the model previously.

--- core-app/db/migrate/20140902210605_init_schema.rb
+++ core-app/db/migrate/20140902210605_init_schema.rb
@@ -168,9 +168,9 @@ class InitSchema < ActiveRecord::Migration
     ### Begin User Tables
 
     create_table "users", :force => true do |t|
-      t.string   "first_name",    :null => false
+      t.string   "first_name"
       t.string   "middle_name"
-      t.string   "last_name",     :null => false
+      t.string   "last_name"
       t.string   "gender"
       t.string   "username",     :null => false
       t.string   "password_digest"

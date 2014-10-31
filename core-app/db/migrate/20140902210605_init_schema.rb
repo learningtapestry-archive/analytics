5dd85ad4aa5c326d4322086ea6b8d0d1c8ed7bb8
Jason <jasonhoekstra@gmail.com>
2014-10-31 17:36:36 -0400

    Adding org fields:  API secret, invalid login count and locked.  Formatting changes for readability.

--- core-app/db/migrate/20140902210605_init_schema.rb
+++ core-app/db/migrate/20140902210605_init_schema.rb
@@ -3,35 +3,38 @@ class InitSchema < ActiveRecord::Migration
 
     ### Begin Message Collection Tables
 
-    create_table "api_keys", :force => true do |t|
-      t.string   "key",          :null => false
-      t.belongs_to  :user,       :null => false
+    create_table 'api_keys', force: true do |t|
+      t.string        'key',          null: false
+      t.belongs_to    :user,          null: false
       t.timestamps
     end
 
-    create_table "organizations", :force => true do |t|
-      t.string "name"
-      t.string "org_api_key"
+    create_table 'organizations', force: true do |t|
+      t.string        'name'
+      t.string        'org_api_key'
+      t.string        'org_secret_key'
+      t.integer       'login_attempts'
+      t.boolean       'locked'
     end
 
-    create_table "raw_messages", :force => true do |t|
-      t.string   "api_key"
-      t.integer  "user_id"
-      t.string   "org_api_key"
-      t.string   "username"
-      t.string   "page_title"
-      t.uuid     "site_uuid"
-      t.string   "verb"
-      t.json     "action"
-      t.string   "url",              :limit => 4096
-      t.datetime "captured_at"
-      t.belongs_to  :user
+    create_table 'raw_messages', force: true do |t|
+      t.string        'api_key'
+      t.string        'org_api_key'
+      t.integer       'user_id'
+      t.string        'username'
+      t.string        'page_title'
+      t.uuid          'site_uuid'
+      t.string        'verb'
+      t.json          'action'
+      t.text          'url'
+      t.datetime      'captured_at'
+      t.belongs_to    :user
       t.timestamps
     end
 
-    create_table "raw_message_logs", :force => true do |t|
-      t.string "action"
-      t.belongs_to :raw_message
+    create_table 'raw_message_logs', force: true do |t|
+      t.string        'action'
+      t.belongs_to    :raw_message
       t.timestamps
     end
 
@@ -39,30 +42,30 @@ class InitSchema < ActiveRecord::Migration
 
     ### Begin Monitored Sites Tables
 
-    create_table "sites", :force => true do |t|
-      t.string   "url",             :null => false, :limit => 4096
-      t.string   "display_name"
-      t.uuid     "site_uuid",       :null => false
-      t.string   "logo_url_small",  :limit => 4096
-      t.string   "logo_url_large",  :limit => 4096
+    create_table 'sites', force: true do |t|
+      t.text          'url',          null: false
+      t.string        'display_name'
+      t.uuid          'site_uuid',    null: false
+      t.text          'logo_url_small'
+      t.text          'logo_url_large'
       t.timestamps
     end
 
     add_index :sites, :url, :unique => true
 
-    create_table "site_actions", :force => true do |t|
-      t.string   "action_type",     :null => false # CLICK, PAGEVIEW, EXTRACT 
-      t.string   "url_pattern",     :null => false, :limit => 4096
-      t.string   "css_selector"
-      t.belongs_to  :site
+    create_table 'site_actions', force: true do |t|
+      t.string        'action_type',  null: false # CLICK, PAGEVIEW, EXTRACT
+      t.text          'url_pattern',  null: false
+      t.string        'css_selector'
+      t.belongs_to    :site
       t.timestamps
     end
 
-    create_table "approved_sites", :force => true do |t|
-      t.belongs_to :site,           :null => false, :index => true
-      t.belongs_to :district
-      t.belongs_to :school
-      t.belongs_to :section
+    create_table 'approved_sites', force: true do |t|
+      t.belongs_to    :site,          null: false, :index => true
+      t.belongs_to    :district
+      t.belongs_to    :school
+      t.belongs_to    :section
       # Potentially course and user, but for the future if needed
       t.timestamps
     end
@@ -71,139 +74,139 @@ class InitSchema < ActiveRecord::Migration
 
     ### Begin Collected Data Schemantic Tables
 
-    create_table "pages", :force => true do |t|
-      t.string   "url",         :null => false, :limit => 4096
-      t.string   "display_name"
-      t.belongs_to :site
+    create_table 'pages', force: true do |t|
+      t.text          'url',          null: false
+      t.string        'display_name'
+      t.belongs_to    :site
       t.timestamps
     end
 
     add_index :pages, :url, :unique => true
 
-    create_table "page_visits", :force => true do |t|
-      t.datetime "date_visited"
-      t.column   "time_active", :interval
-      t.belongs_to :user
-      t.belongs_to :page
+    create_table 'page_visits', force: true do |t|
+      t.datetime      'date_visited'
+      t.column        'time_active',  :interval
+      t.belongs_to    :user
+      t.belongs_to    :page
     end
 
-    create_table "page_clicks", :force => true do |t|
-      t.datetime "date_visited"
-      t.string   "url_visited", :limit => 4096
-      t.belongs_to :user
-      t.belongs_to :page  # In future, this will belong to page_visits once relationships figured out
+    create_table 'page_clicks', force: true do |t|
+      t.datetime      'date_visited'
+      t.text          'url_visited'
+      t.belongs_to    :user
+      t.belongs_to    :page  # In future, this will belong to page_visits once relationships figured out
     end
 
-    ### End Collected Data Schemantic Tables
+    ### End Collected Data Semantic Tables
 
     ### Begin Education Organization and Course Tables
 
-    create_table "districts", :force => true do |t|
-      t.string   "state_id"
-      t.string   "nces_id"
-      t.string   "sis_id"
-      t.string   "other_id"
-      t.string   "name",         :null => false
-      t.string   "address"
-      t.string   "city"
-      t.string   "state"
-      t.string   "phone"
-      t.string   "grade_low"
-      t.string   "grade_high"
+    create_table 'districts', force: true do |t|
+      t.string        'state_id'
+      t.string        'nces_id'
+      t.string        'sis_id'
+      t.string        'other_id'
+      t.string        'name',         null: false
+      t.string        'address'
+      t.string        'city'
+      t.string        'state'
+      t.string        'phone'
+      t.string        'grade_low'
+      t.string        'grade_high'
       t.timestamps
     end
 
-    create_table "schools", :force => true do |t|
-      t.string   "state_id"
-      t.string   "nces_id"
-      t.string   "sis_id"
-      t.string   "other_id"
-      t.string   "name",         :null => false
-      t.string   "address"
-      t.string   "city"
-      t.string   "state"
-      t.string   "phone"
-      t.string   "grade_low"
-      t.string   "grade_high"
-      t.belongs_to :district
+    create_table 'schools', force: true do |t|
+      t.string        'state_id'
+      t.string        'nces_id'
+      t.string        'sis_id'
+      t.string        'other_id'
+      t.string        'name',         null: false
+      t.string        'address'
+      t.string        'city'
+      t.string        'state'
+      t.string        'phone'
+      t.string        'grade_low'
+      t.string        'grade_high'
+      t.belongs_to    :district
       t.timestamps
     end
     
-    create_table "courses", :force => true do |t|
-      t.string   "course_code"
-      t.string   "sis_id"
-      t.string   "other_id"
-      t.string   "name",         :null => false
-      t.string   "description"
-      t.string   "subject_area"
-      t.boolean  "high_school_requirement"
+    create_table 'courses', force: true do |t|
+      t.string        'course_code'
+      t.string        'sis_id'
+      t.string        'other_id'
+      t.string        'name',         null: false
+      t.string        'description'
+      t.string        'subject_area'
+      t.boolean       'high_school_requirement'
       t.timestamps
     end
 
-    create_table "course_offerings", :force => true do |t|
-      t.integer  "course_id",    :null => false
-      t.string   "sis_id"
-      t.string   "other_id"
-      t.date     "date_start"
-      t.date     "date_end"
+    create_table 'course_offerings', force: true do |t|
+      t.integer       'course_id',    null: false
+      t.string        'sis_id'
+      t.string        'other_id'
+      t.date          'date_start'
+      t.date          'date_end'
       t.timestamps
     end
 
-    create_table "sections", :force => true do |t|
-      t.string   "section_code"
-      t.integer  "course_offering_id"
-      t.string   "sis_id"
-      t.string   "other_id"
-      t.string   "name",         :null => false
+    create_table 'sections', force: true do |t|
+      t.string        'section_code'
+      t.integer       'course_offering_id'
+      t.string        'sis_id'
+      t.string        'other_id'
+      t.string        'name',        null: false
       t.timestamps
     end
 
-    create_table "section_users", :force => true do |t|
-      t.string   "user_type" # defines type of relationship user has to section (e.g.: "teacher" "student" "TA" "auditing")
-      t.belongs_to :section
-      t.belongs_to :user
+    create_table 'section_users', force: true do |t|
+      t.string        'user_type' # defines type of relationship user has to section (e.g.: 'teacher' 'student' 'TA' 'auditing')
+      t.belongs_to    :section
+      t.belongs_to    :user
     end
 
     ### End Education Organization and Course Tables
 
     ### Begin User Tables
 
-    create_table "users", :force => true do |t|
-      t.string   "first_name"
-      t.string   "middle_name"
-      t.string   "last_name"
-      t.string   "gender"
-      t.string   "username",     :null => false
-      t.string   "password_digest"
-      t.date     "date_of_birth"
-      t.belongs_to  :school
-      t.belongs_to  :organization
+    create_table 'users', force: true do |t|
+      t.string        'first_name'
+      t.string        'middle_name'
+      t.string        'last_name'
+      t.string        'gender'
+      t.string        'username',     null: false
+      t.string        'password_digest'
+      t.date          'date_of_birth'
+      t.belongs_to    :school
+      t.belongs_to    :organization
       t.timestamps
     end
 
     add_index :users, :username, :unique => true
 
-    create_table "emails", :force => true do |t|
-      t.string   "email"
-      t.boolean  "primary"
-      t.belongs_to :user
+    create_table 'emails', force: true do |t|
+      t.string        'email'
+      t.boolean       'primary'
+      t.belongs_to    :user
     end
 
-    create_table "students", :force => true do |t|
-      t.string   "state_id"
-      t.string   "sis_id"
-      t.string   "other_id"
-      t.string   "grade_level"
-      t.belongs_to :user
+    create_table 'students', force: true do |t|
+      t.string        'state_id'
+      t.string        'sis_id'
+      t.string        'other_id'
+      t.string        'grade_level'
+      t.belongs_to    :user
       t.timestamps
     end
 
-    create_table "staff_members", :force => true do |t|
-      t.string   "state_id"
-      t.string   "sis_id"
-      t.string   "other_id"
-      t.string   "staff_member_type", :null => false
-      t.belongs_to :user
+    create_table 'staff_members', force: true do |t|
+      t.string        'state_id'
+      t.string        'sis_id'
+      t.string        'other_id'
+      t.string        'staff_member_type', null: false
+      t.belongs_to    :user
       t.timestamps
     end 
 

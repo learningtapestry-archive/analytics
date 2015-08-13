c9d5d9d33c1db2a27c8960eb2131cdf61ae92b1b
Abraham Sánchez <abraham@twintags.com>
2015-08-13 16:37:18 +0200

    Use a separate log for the cronjobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,7 @@ require_relative 'environment'
 
 set :environment_variable, 'RACK_ENV'
 set :environment, LT.env.run_env
-set :output, "#{LT.env.log_path}/lt-#{LT.env.run_env}.log"
+set :output, "#{LT.env.log_path}/lt-cron.log"
 
 every 5.minutes do
   rake 'lt:janitors:import_raw_messages'

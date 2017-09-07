735c16ce2d52a370e8e5b6987ddc2aed5725d8e2
Alex Nizamov <nizamov.alex@gmail.com>
2017-09-07 18:00:59 +0500

    Temporarily disable slow tasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,6 +8,6 @@ set :output, "#{LT.env.log_path}/lt-cron.log"
 
 every 5.minutes do
   rake 'lt:janitors:import_raw_messages'
-  rake 'lt:janitors:extract_page_visits'
-  rake 'lt:janitors:extract_video_views'
+  # rake 'lt:janitors:extract_page_visits'
+  # rake 'lt:janitors:extract_video_views'
 end
